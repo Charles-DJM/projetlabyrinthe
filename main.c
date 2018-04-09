3dde5bdffab2c408adb8b8bae7d0a200f8a05a57
bobvouvouvou <bcraaazy@gmail.com>
2018-04-09 17:27:33 +0200

    main

--- main.c
+++ main.c
@@ -10,7 +10,7 @@ int main (void)
 int n ; //Taille du labyrinthe
 
 n = Taille () ;
-int MUR [500][500] = {1} ; // Défini l'état des murs
+int MUR [100][100] = {1} ; // Défini l'état des murs
 int x=30 , y= (n*60)-30, x2 = 0, y2 = n ;
 start_graphics() ;
 
@@ -18,8 +18,12 @@ Grille (n) ;
 Creer_Curseur (&x, &y) ;
 Mouvement_Creation (&x, &y, &x2, &y2, n, MUR) ;
 
+f = fopen ("Labyrinthes.txt", "w") ;
+Enregistrement (f) ;
+
 get_key () ;
 
 stop_graphics () ;
+fclose (f) ;
 return 0 ;
 }
